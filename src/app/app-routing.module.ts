424e79bb694fa761b8a6b0c571e3a96cc62e61f3
pasindu <bgpsandaruwan@gmail.com>
2020-02-28 23:39:39 +0530

    create project for code complexity tool

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UploadComponent} from "./upload/upload.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'upload',
+    component: UploadComponent,
+    children: [
+      // {
+      //   path: 'employee',
+      //   component: EmployeeComponent
+      // }
+    ]
+  },
+
+  {path: '', pathMatch: "full", redirectTo: '/upload'}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
